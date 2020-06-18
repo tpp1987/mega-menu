533608d15d423859d79d43e327f2cc1641bba8c8
Tom Parkes <tomparkes1987@hotmail.co.uk>
2020-06-18 21:15:38 +0100

    Fixed mouseleave behaviour

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const subMenu = document.querySelector(".menu-sub")
+const menuMain = document.querySelector(".menu-main")
 
 const linkOne = document.querySelector("#link-one")
 const linkTwo = document.querySelector("#link-two")
@@ -13,53 +13,41 @@ const subFour = document.querySelector("#sub-four")
 const overlay = document.querySelector(".overlay")
 
 linkOne.addEventListener("mouseover", function (e) {
-    console.log("mouse over")
     overlay.classList.replace("hidden", "visible")
 })
 
 subOne.addEventListener("mouseleave", function (e) {
-    console.log("mouse leave")
     overlay.classList.replace("visible", "hidden")
 })
 
 linkTwo.addEventListener("mouseover", function (e) {
-    console.log("mouse over")
     overlay.classList.replace("hidden", "visible")
 })
 
 subTwo.addEventListener("mouseleave", function (e) {
-    console.log("mouse leave")
     overlay.classList.replace("visible", "hidden")
 })
 
 linkThree.addEventListener("mouseover", function (e) {
-    console.log("mouse over")
     overlay.classList.replace("hidden", "visible")
 })
 
 subThree.addEventListener("mouseleave", function (e) {
-    console.log("mouse leave")
     overlay.classList.replace("visible", "hidden")
 })
 
 linkFour.addEventListener("mouseover", function (e) {
-    console.log("mouse over")
     overlay.classList.replace("hidden", "visible")
 })
 
 subFour.addEventListener("mouseleave", function (e) {
-    console.log("mouse leave")
     overlay.classList.replace("visible", "hidden")
 })
 
 document.querySelector("html").addEventListener("mouseleave", function (e) {
-    console.log("mouse leave")
     overlay.classList.replace("visible", "hidden")
 })
 
-// Previous working version with only one link
-
-// subMenu.addEventListener("mouseleave", function (e) {
-//     console.log("mouse leave")
-//     overlay.classList.replace("visible", "hidden")
-// })
+menuMain.addEventListener("mouseleave", function (e) {
+    overlay.classList.replace("visible", "hidden")
+})
